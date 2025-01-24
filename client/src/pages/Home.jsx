a1653c8c8621bf03870bd1e1696752a2f6d9f411
shwetangdalvi <shwetang.dalvi1991@gmail.com>
2025-01-24 15:36:27 -0500

    upgrade

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,58 +1,219 @@
-import React, { useState, useEffect } from 'react'
-import { Loader, Card, FormField } from '../components'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const RenderCards = ({ data, title }) => {
-    if (data?.length > 0) {
-        return data.map((post) => ( <Card key={post._id} {...post} /> ));
+import { preview } from '../assets';
+import { getRandomPrompt } from '../utils';
+import { FormField, Loader } from '../components';
+
+const CreatePost = () => {
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+    model: 'flux',
+    width: 1024,
+    height: 1024,
+  });
+
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt();
+    setForm({ ...form, prompt: randomPrompt });
+};
+
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            prompt: form.prompt,
+          }),
+        });
+
+
+        const data = await response.json();
+        console.log(data);
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+      } catch (err) {
+        alert(err);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert('Please provide proper prompt');
     }
-    return (
-        <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">{title}</h2>
-    )
-}
+  };
 
-const Home = () => {
-    const [loading, setLoading] = useState(false);
-    const [allPosts, setAllPosts] = useState(null);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const [searchText, setSearchText] = useState('');
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ ...form }),
+        });
+
+        await response.json();
+        alert('Success');
+        navigate('/');
+      } catch (err) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert('Please generate an image with proper details');
+    }
+  };
+
+  const downloadImage = () => {
+    const link = document.createElement('a');
+    link.href = form.photo;
+    link.download = 'generated-image.jpg';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
-    <section className="max-w-3xl mx-auto">
-        <div>
-            <h1 className="font-extrabold text-[#222328] text-[32px]">The Community Showcase</h1>
-            <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">Welcome to the community showcase. Here you can see all the amazing projects that the community has been working on. Feel free to share your own project as well.</p>
+    <section className="max-w-7xl mx-auto">
+      <div >
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">Generate an imaginative image through DALL-E AI and share it with the community</p>
+      </div>
+
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
+            labelName="Your Name"
+            type="text"
+            name="name"
+            placeholder="Elon Musk"
+            value={form.name}
+            handleChange={handleChange}
+          />
+
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="An Impressionist oil painting of sunflowers in a purple vase…"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+          <div className="flex gap-5 items-center bg-gray-100 p-4 rounded-lg shadow-md">
+            <div className='flex-1'>
+              <label className="block text-sm font-medium text-gray-700">Model</label>
+              <select
+                  name="model"
+                  value={form.model}
+                  onChange={handleChange}
+                  className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
+              >
+                  {[
+                      "flux",
+                      "flux-realism",
+                      "flux-cablyai",
+                      "flux-anime",
+                      "flux-3d",
+                      "any-dark",
+                      "flux-pro",
+                      "turbo"
+                  ].map((model) => (
+                      <option key={model} value={model}>
+                          {model}
+                      </option>
+                  ))}
+              </select>
+          </div>
+          <FormField 
+              labelName="Width"
+              type="number"
+              name="width"
+              placeholder="1024"
+              value={form.width}
+              onChange={handleChange}
+          />
+          <FormField 
+              labelName="Height"
+              type="number"
+              name="height"
+              placeholder="1024"
+              value={form.height}
+              onChange={handleChange}
+          />
+          </div>
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            { form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+
+            {generatingImg && (
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                <Loader />
+              </div>
+            )}
+          </div>
         </div>
 
-        <div className="mt-16">
-            <FormField />
+        <div className="mt-5 flex gap-5">
+          <button
+            type="button"
+            onClick={generateImage}
+            className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {generatingImg ? 'Generating...' : 'Generate'}
+          </button>
+          {form.photo && (
+              <button 
+                  type="button"
+                  onClick={downloadImage}
+                  className="text-white bg-blue-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center">
+                  Download Image
+              </button>
+          )}
         </div>
 
         <div className="mt-10">
-        {loading ? (
-            <div className="flex justify-center items-center">
-                <Loader />
-            </div>
-        ) : (
-            <>
-            {searchText && (
-                <h2 className="font-medium text-[#666e75] text-xl mb-3">Search results for "{searchText}"</h2>
-            )}
-            <div className="grid lg:grid-cols-4 md:grid-cols-3 xs:grid-col-2 grid-cols-1 gap-6">
-                {searchText ? (
-                    <RenderCards 
-                        data={[]}
-                        title="No Search results found" />
-                ) : (
-                    <RenderCards 
-                        data={[]}
-                        title="No posts found" />
-                )}
-            </div>
-
-            </>
-        )}
+          <p className="mt-2 text-[#666e75] text-[14px]">** Once you have created the image you want, you can share it with others in the community **</p>
+          <button
+            type="submit"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {loading ? 'Sharing...' : 'Share with the Community'}
+          </button>
         </div>
+      </form>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default CreatePost;
